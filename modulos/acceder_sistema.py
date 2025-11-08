c5d06186b0ca6fe577f27f58068be66ae8058fbf
Yely-cyber <230252@unsaac.edu.pe>
2025-11-07 22:27:43 -0500

    Delete modulos directory

--- modulos/acceder_sistema.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-def acceder_sistema(email):
-    token = secrets.token_hex(16)
-    print(f"Usuario {email} accedió al sistema. Token: {token}")
-    return token
--- modulos/validar_usuario.py
+++
@@ -1,2 +0,0 @@
-def validar_usuario(email):
-    return email.endswith("@unsaac.edu.pe")
